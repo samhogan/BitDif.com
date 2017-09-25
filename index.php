1ad054d46131b2e3eaf90dbad99cff5bcbcbf0d8
samhogan <sam@samhogan.net>
2017-09-24 20:22:21 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+ 	<div class="container">
+		<form> <!-- class="form-inline"> -->
+			<div class="form-group row justify-content-md-center">
+				<div class="col-lg-3">
+		      		<label class="sr-only" for="crypto1">Example select</label>
+	      			<select class="form-control" id="crypto1">
+	        			<option>100000</option>
+	        			<option>2</option>
+				        <option>3</option>
+				        <option>4</option>
+				        <option>5</option>
+				    </select>
+				</div>
+				<div class="col-lg-3">
+			    	<label class="sr-only" for="crypto2">Example select</label>
+			      	<select class="form-control mb-2 mr-sm-2 mb-sm-0" id="crypto2">
+				        <option>1</option>
+				        <option>2</option>
+				        <option>3</option>
+				        <option>4</option>
+				        <option>5</option>
+			      	</select>
+	    		</div>
+	  		</div>
+
+	  		<button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+
+  	</div>
+
+	<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script> -->
+</body>
+</html>
+
