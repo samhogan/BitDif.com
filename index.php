2c08186d93a1fa5e3550dd8cd507051330aa5a40
samhogan <sam@samhogan.net>
2017-09-25 20:56:36 -0400

    main form progress

--- index.php
+++ index.php
@@ -8,34 +8,69 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
 	<link rel="stylesheet" type="text/css" href="style.css">
+	
 </head>
 <body>
+	<!-- navbar -->
+    <nav class='navbar navbar-expand-lg navbar-light bg-light mb-4'>
+    	<div class="container">
+    		<a class="navbar-brand" href="#">BitDif</a>
+
+    		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
+	          	<span class="navbar-toggler-icon"></span>
+	        </button>
+
+    		<div class="collapse navbar-collapse" id="nav-content">
+    			<ul class="navbar-nav mr-auto">	
+    				<li class="navbar-nav">
+    					<a class="nav-link" href="#">Compare<!-- <span class="sr-only">(current)</span> --></a>
+    				</li>
+
+    			</ul>
+
+    		</div>
+    	</div>
+
+    </nav>
+
+
+
+	<!-- content -->
  	<div class="container">
+ 		<div class="col-lg-12 outline mb-4">
+ 			<h3 class="text-center">Compare Cryptocurrencies</h3>
+ 		</div>
 		<form> <!-- class="form-inline"> -->
 			<div class="form-group row justify-content-md-center">
-				<div class="col-lg-3">
+				<div class="col-md-3 outline">
 		      		<label class="sr-only" for="crypto1">Example select</label>
 	      			<select class="form-control" id="crypto1">
-	        			<option>100000</option>
-	        			<option>2</option>
-				        <option>3</option>
-				        <option>4</option>
-				        <option>5</option>
+	        			<option>Bitcoin</option>
+	        			<option>Litecoin</option>
+				        <option>Ripple</option>
+				        <option>Dash</option>
+				        <option>Zcash</option>
 				    </select>
 				</div>
-				<div class="col-lg-3">
+				<div class="col-md-1 outline">
+					<p class="vs mb-0">vs</p>
+				</div>
+				<div class="col-md-3 outline">
 			    	<label class="sr-only" for="crypto2">Example select</label>
-			      	<select class="form-control mb-2 mr-sm-2 mb-sm-0" id="crypto2">
-				        <option>1</option>
-				        <option>2</option>
-				        <option>3</option>
-				        <option>4</option>
-				        <option>5</option>
+			      	<select class="form-control " id="crypto2">
+				        <option>Bitcoin</option>
+	        			<option>Litecoin</option>
+				        <option>Ripple</option>
+				        <option>Dash</option>
+				        <option>Zcash</option>
 			      	</select>
 	    		</div>
 	  		</div>
-
-	  		<button type="submit" class="btn btn-primary">Submit</button>
+	  		<div class="form-group row justify-content-md-center">
+				<div class="col-md-2 outline offset-sm-2 text-center">
+	  				<button type="submit" class="btn btn-primary btn-lg">Compare</button>
+	  			</div>
+	  		</div>
 		</form>
 
   	</div>
