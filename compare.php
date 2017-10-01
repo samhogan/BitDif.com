3b6136a93a5a27aa766f9cef788ab1b4662ff3d4
samhogan <sam@samhogan.net>
2017-10-01 19:54:57 -0400

    market data, styles, more fields

--- compare.php
+++ compare.php
@@ -5,11 +5,11 @@
 	include("cryptodat.php");
 
 	//echo $_GET["crypto2"];
-	$crypto1 = $crypto2 = "";
+	$cryptoID1 = $cryptoID2 = "";
 
 	if ($_SERVER["REQUEST_METHOD"] == "GET") {
-	  $crypto1 = test_input($_GET["crypto1"]);
-	  $crypto2 = test_input($_GET["crypto2"]);
+	  $cryptoID1 = test_input($_GET["crypto1"]);
+	  $cryptoID2 = test_input($_GET["crypto2"]);
 	}
 
 	function test_input($data) {
@@ -20,9 +20,17 @@
 	  return $data;
 	}
 
-	$title = $cryptos[$crypto1]["name"] . " vs " . $cryptos[$crypto2]["name"];
+	$crypto1 = $cryptos[$cryptoID1];
+	$crypto2 = $cryptos[$cryptoID2];
+
+
+	$title = $crypto1["name"] . " vs " . $crypto2["name"];
+
+	$marketData1 = json_decode(file_get_contents("http://coincap.io/page/" . $crypto1["ccsymb"]), true);
+	$marketData2 = json_decode(file_get_contents("http://coincap.io/page/" . $crypto2["ccsymb"]), true);
 
 ?>
+
 	<h1 id="title"><?php echo $title; ?>: What's the Difference?</h1>
 	<div class="container">
 	
@@ -31,39 +39,84 @@
 				
 			</div>
 			<div class="col-md-3 col-6 outline p-2 text-center">
-				<img src=<?php echo "images/" . $cryptos[$crypto1]["img"] ?> class="icon">
-				<h3 class="pt-3"><?php echo $cryptos[$crypto1]["name"] ?></h3>
+				<img src=<?php echo "images/" . $crypto1["img"] ?> class="icon">
+				<h3 class="pt-3"><?php echo $crypto1["name"] ?></h3>
 			</div>
 			<div class="col-md-3 col-6 outline p-2 text-center">
-				<img src=<?php echo "images/" . $cryptos[$crypto2]["img"] ?> class="icon">
-				<h3 class="pt-3"><?php echo $cryptos[$crypto2]["name"] ?></h3>
+				<img src=<?php echo "images/" . $crypto2["img"] ?> class="icon">
+				<h3 class="pt-3"><?php echo $crypto2["name"] ?></h3>
 			</div>
 
 		</div>
 
 		<?php
+			$light = true;
 			foreach ($fields as $field) 
 			{
 				$id = $field["id"];
 			    $name = $field["name"];
-			    $val1 = $cryptos[$crypto1][$id];
-			    $val2 = $cryptos[$crypto2][$id];
+			    $val1 = $val2 = "";
+			    if(array_key_exists("ccf", $field))
+			    {
+			    	$val1 = $marketData1[$field["ccf"]];
+			    	$val2 = $marketData2[$field["ccf"]];
+
+			    	$dec = $field["decimals"];
+			    	$val1 = round($val1, $dec);
+			    	$val2 = round($val2, $dec);
+
+			    	//format with commas in thousands
+			    	if($val1>10000 || $val2>10000)
+			    	{
+			    		$val1 = number_format($val1, $dec, '.', ',');
+			    		$val2 = number_format($val2, $dec, '.', ',');
+			    	}
+			    	else
+			    	{
+			    		$val1 = number_format($val1, $dec, '.', '');
+			    		$val2 = number_format($val2, $dec, '.', '');
+			    	}
+
+			    	if(array_key_exists("prefix", $field))
+			    	{
+
+			    		$val1 = $field["prefix"] . $val1;
+			    		$val2 = $field["prefix"] . $val2;
+			    		
+			    	}
+
+			    }
+			    else
+			    {
+				    $val1 = $crypto1[$id];
+				    $val2 = $crypto2[$id];	
+				}
+				$boxColor = $light ? "grid-box-light" : "grid-box-dark";
+				$classes1 = "grid-box px-0 py-1 vcenter hcenter $boxColor";
+				$classes2 = "grid-box px-0 py-3 vcenter hcenter $boxColor";
+				$classes3 = "grid-box px-0 py-1 vcenter hcenter $boxColor";
+
+				if(array_key_exists("longText", $field))
+				{
+					$classes2 = "grid-box px-2 py-2 vcenter hcenter $boxColor";
+					$classes3 = "grid-box px-2 py-2 vcenter hcenter $boxColor";
+				}
 
 		?>
 			
 		<div class="row justify-content-md-center grid-row no-gutters">
 			<div class="col-md-3 outline p-2">
-				<div class="grid-box px-3 py-1 vcenter">
+				<?php echo "<div class='$classes1'>" ?>
 					<p class="field"><?php echo $name ?></p>
 				</div>
 			</div>
 			<div class="col-md-3 col-6 outline p-2">
-				<div class="grid-box px-3 py-3 vcenter">
+				<?php echo "<div class='$classes2'>" ?>
 					<p><?php echo $val1 ?></p>
 				</div>
 			</div>
 			<div class="col-md-3 col-6 outline p-2">
-				<div class="grid-box px-3 py-1 vcenter">
+				<?php echo "<div class='$classes3'>" ?>
 					<p><?php echo $val2 ?></p>
 				</div>
 			</div>
@@ -71,7 +124,7 @@
 		</div>
 
 		<?php
-
+			$light = !$light;	
 		}
 		
 		?>
--- cryptodat.php
+++ cryptodat.php
@@ -4,31 +4,48 @@
 
 		"bitcoin" => array(
 			"name" => "Bitcoin",
-			"desc" => "The first and largest peer-to-peer cryptocurrency",
+			"desc" => "The first and largest decentralized cryptocurrency",
 			"price" => "\$something",
 			"circ" => 16700000,
-			"max" => 20999999.9769,
+			"max" => "~21,000,000",//"20,999,999.9769",
 			"marketcap" => "like, a lot",
 			"ticker" => "BTC or XBT",
-			"prooftype" => "Proof of Work",
+			"prooftype" => "Proof-of-Work",
 			"hash" => "SHA-256",
 			"img" => "Bitcoin.svg",
+			"ccsymb" => "BTC",
+
+			"block-time" => "10 minutes",
+			"block-size" => "1MB",
+			"block-reward" => "12.5 BTC",
+
+			"hardware" => "ASIC",//primary mining hardware
+
+			"genesis" => "2009-01-03",
 
 
 		),
 
 		"litecoin" => array(
 			"name" => "Litecoin",
-			"desc" => "One of the first altcoins that aimed to improve upon the technology of Bitcoin",
+			"desc" => "One of the first altcoins; aimed to improve upon the technology of Bitcoin",
 			"price" => "\$something",
 			"circ" => 53000000,
-			"max" => 84000000,
+			"max" => "~84,000,000",
 			"marketcap" => "less, but still a lot",
 			"ticker" => "LTC",
-			"prooftype" => "Proof of Work",
+			"prooftype" => "Proof-of-Work",
 			"hash" => "Scrypt",
 			"img" => "Litecoin.svg",
+			"ccsymb" => "LTC",
 
+			"block-time" => "2.5 minutes",
+			"block-size" => "1MB",
+			"block-reward" => "25 LTC",
+
+			"hardware" => "ASIC",
+
+			"genesis" => "2011-10-07",
 
 
 		),
@@ -46,28 +63,73 @@
 	$fields = array(
 			array(
 				"id" => "desc",
-				"name" => "Description"
+				"name" => "Description",
+				"longText" => true,
 			),
 			array(
 				"id" => "price",
-				"name" => "Price"
+				"name" => "Price (USD)",
+				"ccf" => "price_usd",
+				"prefix" => "\$",
+				"decimals" => 2,
+			),
+			array(
+				"id" => "marketcap",
+				"name" => "Market Cap",
+				"ccf" => "market_cap",
+				"prefix" => "\$",
+				"decimals" => 2,
+			),
+			array(
+				"name" => "24hr Volume",
+				"ccf" => "volume",
+				"prefix" => "\$",
+				"decimals" => 2,
 			),
 			array(
 				"id" => "circ",
 				"name" => "Current Supply",
+				"ccf" => "supply",
+				"decimals" => 0,
 			),
 			array(
 				"id" => "max",
 				"name" => "Maximum Supply",
 			),
 			array(
-				"id" => "marketcap",
-				"name" => "Market Cap",
-			),
-			array(
 				"id" => "ticker",
 				"name" => "Ticker Symbol",
 			),
+			array(
+				"id" => "prooftype",
+				"name" => "Proof Type",
+			),
+			array(
+				"id" => "hash",
+				"name" => "Hash Function",
+			),
+			array(
+				"id" => "block-time",
+				"name" => "Block Time",
+			),
+			array(
+				"id" => "block-size",
+				"name" => "Max Block Size",
+			),
+			array(
+				"id" => "block-reward",
+				"name" => "Current Block Reward",
+			),
+			array(
+				"id" => "hardware",
+				"name" => "Primary Mining Hardware",
+			),
+			array(
+				"id" => "genesis",
+				"name" => "Genesis Block",
+			),
+			
+			
 
 
 
